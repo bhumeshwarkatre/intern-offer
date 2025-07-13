93093b559dcea41a65c5c0c21a8088ec499136c1
Bhumeshwar Katre <132291125+bhumeshwarkatre@users.noreply.github.com>
2025-07-13 17:00:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ with st.expander("🔐 Admin Panel"):
         else:
             st.info("CSV log not found.")
 
-st.divider()
+        st.divider()
         
         st.markdown("<h3 style='color:#1E88E5;'>📥 One-Time CSV Upload <small style='color:gray;'>(Optional)</small></h3>", unsafe_allow_html=True)   
         uploaded_csv = st.file_uploader("Upload Existing Intern CSV", type=["csv"])
